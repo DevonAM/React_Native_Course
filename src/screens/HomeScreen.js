09517e7d484d6223715b5b6311c0afeac3bfab8a
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-03 16:03:41 -0700

    add nav buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Haillll yeeeeeh m8</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="I'm a button"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      <Button
+        title="Go to Images"
+        onPress={() => navigation.navigate("Images")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
-
 export default HomeScreen;
